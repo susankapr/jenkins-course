acb32781a8528cb0ab206d3fed5b7465b64d7d1a
susankapr <susan.kapr@gmail.com>
2021-09-24 09:41:58 -0400

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('susankapr/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('susankapr')
+            registryCredentials('nicolezmomma')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
