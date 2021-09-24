3cb3d4d247614f792f9c0e1d187b78fbddaea993
susankapr <susan.kapr@gmail.com>
2021-09-24 08:04:46 -0400

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('susankapr')
+            node / gitConfigEmail('susan.kapr@gmail.com')
         }
     }
     triggers {
